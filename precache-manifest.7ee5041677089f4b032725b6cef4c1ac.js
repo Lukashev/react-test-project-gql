bec821604725ffd844e0172f84cb470f105f292f
Nikita <lnv161297@gmail.com>
2020-06-03 10:01:43 +0300

    Updates

---
+++ precache-manifest.7ee5041677089f4b032725b6cef4c1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99421a58e4cc5d301f84f790c1b87bc8",
+    "url": "/react-test-project-gql/index.html"
+  },
+  {
+    "revision": "15c532d09fe178264584",
+    "url": "/react-test-project-gql/static/css/main.7ecf9349.chunk.css"
+  },
+  {
+    "revision": "498648c7b8d7aa5ba4ef",
+    "url": "/react-test-project-gql/static/js/2.59297334.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/react-test-project-gql/static/js/2.59297334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c532d09fe178264584",
+    "url": "/react-test-project-gql/static/js/main.dd018d9c.chunk.js"
+  },
+  {
+    "revision": "9435fd18b78ec0674938",
+    "url": "/react-test-project-gql/static/js/runtime-main.5c027570.js"
+  }
+]);
